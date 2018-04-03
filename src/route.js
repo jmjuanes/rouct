a802701ee962485c82339fbed1c847499ef02aec
jmjuanes <josemijuanes@gmail.com>
2018-04-03 11:27:50 +0200

    Init route component

---
+++ src/route.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//Route class
+export default class Route extends React.Component {
+    render() {
+        return React.createElement("div", {}, null);
+    }
+}
+
+//Route default props
+Route.defaultProps = {path: "*", component: null, props: {}};
