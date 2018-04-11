8777b9c19b9a3feb162814d78891fad125814059
jmjuanes <josemijuanes@gmail.com>
2018-04-11 10:53:44 +0200

    Export app component

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+import App from "./src/app.js";
 import redirect from "./src/redirect.js";
 import Route from "./src/route.js";
 import Switch from "./src/switch.js";
 
 export {
+    App,
     redirect,
     Route,
     Switch
