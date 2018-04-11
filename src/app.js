5f83e095fd6a183f030b2221513ea45b30e0a1fd
jmjuanes <josemijuanes@gmail.com>
2018-04-11 16:09:18 +0200

    Minor changes

--- src/app.js
+++ src/app.js
@@ -4,21 +4,12 @@ import React from "react";
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-    }
-
-    componentDidMount() {
         let self = this;
         //Add haschange event listener
         window.addEventListener("hashchange", function () {
             //Url changed --> update component
-            //console.log("Updated router app");
             return self.forceUpdate();
         }, false);
-    }
-
-    render() {
-        //Return the children passed to router app without adding new dom nodes
-        return React.createElement(React.Fragment, {}, this.props.children);
     }
 }
 
