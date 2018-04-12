a0393f337b4a778ecb260614f8e9352868cb97d7
jmjuanes <josemijuanes@gmail.com>
2018-04-12 10:15:51 +0200

    Parse provided path before changing the hash string

--- src/redirect.js
+++ src/redirect.js
@@ -1,5 +1,18 @@
 //Redirect to a given path
 export default function redirect(path) {
-    window.location.hash = "#!" + path;
+    if (typeof path !== "string") {
+        //Invalid path
+        throw new Error("Expected string path in redirect method");
+    }
+    //Parse the path string --> trim the path 
+    let parsedPath = path.trim();
+    //while (parsedPath.charAt(0) === "#" || parsedPath.charAt(0) === "!") {
+    //    parsedPath = parsedPath.substring(1);
+    //}
+    //Check if the first character is not a slash mark
+    if (parsedPath.charAt(0) !== "/") {
+        parsedPath = "/" + parsedPath;
+    }
+    window.location.hash = "#!" + parsedPath;
 }
 
