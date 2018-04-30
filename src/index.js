f92834f67201ba0120fd2709a8459c5113eab3b4
jmjuanes <josemijuanes@gmail.com>
2018-04-30 18:43:48 +0200

    Export Router component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import HashbangRouter from "./hashbang-router.js";
 import {redirectHashbang} from "./redirect.js";
 import Route from "./route.js";
+import Router from "./router.js";
 import Switch from "./switch.js";
 
 export {
     HashbangRouter,
     redirectHashbang,
     Route,
+    Router,
     Switch
 };
 
