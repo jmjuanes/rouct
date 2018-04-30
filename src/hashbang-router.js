59933e5e6bbf24845d2e0c349c9741579290f35b
jmjuanes <josemijuanes@gmail.com>
2018-04-30 17:56:22 +0200

    Big rewrite

--- src/hashbang-router.js
+++ src/hashbang-router.js
@@ -1,24 +1,62 @@
 import React from "react";
+import Router from "./router.js";
+//
+//Unescape a character
+let unescape = function (str) {
+    return window.decodeURIComponent(str.replace(/\+/g, " "));
+};
+
+//Get the current hash
+let currentHash = function () {
+    //Get the current hash value
+    let hash = unescape(window.location.hash.substring(1));
+    //Check for empty hash --> home route
+    if (hash.trim() === "") {
+        return "/";
+    }
+    //Check if is a vaid hashbang url
+    return (hash.charAt(0) === "!") ? hash.substring(1) : "/";
+};
 
 //Hashbang router component
 export default class HashbangRouter extends React.Component {
     constructor(props) {
         super(props);
         let self = this;
-        //Add haschange event listener
-        window.addEventListener("hashchange", function () {
-            //Url changed --> update component
-            return self.forceUpdate();
-        }, false);
+        //Initialize the state with the current hashbang location
+        this.state = {
+            "location": currentHash()
+        };
+        //Bind hash change method
+        this.handleHashChange = this.handleHashChange.bind(this);
     }
-    
+
+    handleHashChange() {
+        //Update the state with the new location
+        return this.setState({
+            "location": currentHash();
+        });
+    }
+
+    componentDidMount () {
+        let self = this;
+        //Start listening hash change events
+        window.addEventListener("hashchange", self.handleHashChange, false);
+    }
+
+    componentWillUnmount() {
+        let self = this;
+        //Remove the hashchange event listener
+        window.removeEventListener("hashchange", self.handleHashChange); 
+    }
+
     shouldComponentUpdate() {
         return true;
     }
 
     render() {
-        //Render the children without adding extra nodes to the DOM
-        return React.createElement(React.Fragment, {}, this.props.children);
+        //Render the router component
+        return React.createElement(Router, {location: this.state.location}, this.props.children);
     }
 }
 
