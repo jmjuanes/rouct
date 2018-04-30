0a50962c0cde571401c7be02f968a17462292e0a
jmjuanes <josemijuanes@gmail.com>
2018-04-30 19:01:24 +0200

    Added script to check if the current location matches the route's path

---
+++ src/commons/match.js
@@ -0,0 +1,44 @@
+//Parse a path
+let parsePath = function (str) {
+    let items = str.trim().split("/");
+    if (items[0].trim().length === 0) {
+        items.shift();
+    }
+    if (items[items.length - 1].trim().length === 0) {
+        items.pop();
+    }
+    //items.map(function(item){ return item.trim(); });
+    return items;
+};
+
+//Match function
+export default function match (current, pattern, exact) {
+    //Initialize the params object
+    let params = {};
+    //Check for catch-all path
+    if (pattern === "*") {
+        return {matches: true, params: {}};
+    }
+    //Parse the current and the pattern paths
+    let currentItems = parsePath(current);
+    let patternItems = parsePath(pattern);
+    //Check the number of path items
+    if (exact === true && (currentItems.length !== patternItems.length)) {
+        return {matches: false};
+    }
+    //Check all path items
+    for (let i = 0; i < patternItems.length; i++) {
+        //Check for dynamic parameter
+        if (patternItems[i].charAt(0) === ":") {
+            let paramKey = patternItems[i].substring(1);
+            params[paramKey] = currentItems[i];
+        }
+        //Check for not equal path segment
+        else if (currentItems[i] !== patternItems[i]) {
+            return {matches: false};
+        }
+    }
+    //Paths matches
+    return {matches: true, params: params};
+}
+
