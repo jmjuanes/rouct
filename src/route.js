129140706ffd7faf8360178fefa51134a7240a4f
jmjuanes <josemijuanes@gmail.com>
2018-04-30 18:42:46 +0200

    Big rewrite --> allow to use Route components outside of s Switch components

--- src/route.js
+++ src/route.js
@@ -1,19 +1,38 @@
 import React from "react";
+import {Consumer} from "./context.js";
+import match from "./commons/match.js";
 
 //Route class
 export default class Route extends React.Component {
     render() {
-        //This component should never be rendered
-        console.error("Warning: Route component should be used inside a Switch component");
-        return null;
+        let self = this;
+        //Check for invalid component 
+        if (this.props.component === null) {
+            return null;
+        }
+        //Render the consumer component
+        return React.createElement(Consumer, {}, function (value) {
+            //Check if the path of the route matches the current location
+            let result = match(value.pathname, self.props.path, self.props.exact);
+            if (result.matches === true) {
+                //Generate the request object
+                let request = Object.assign({params: result.params}, value);
+                //Generate the new component props 
+                let props = Object.assign({request: request}, self.props.props);
+                //Return the component
+                return React.createElement(self.props.component, props);
+            }
+            //If not matches, return null
+            return null;
+        });
     }
 }
 
 //Route default props
 Route.defaultProps = {
-    path: "*", 
-    component: null, 
-    props: {},
-    exact: false
+    "path": "*", 
+    "component": null, 
+    "props": {},
+    "exact": false
 };
 
