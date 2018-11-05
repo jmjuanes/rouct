77e0cbc7a83ff700ba4ad8aaedcbb45a02f618dc
jmjuanes <josemijuanes@gmail.com>
2018-11-05 16:47:45 +0100

    Big rewrite. Export browser router and redirect method

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import HashbangRouter from "./hashbang-router.js";
-import {redirectHashbang} from "./redirect.js";
-import Route from "./route.js";
-import Router from "./router.js";
-import Switch from "./switch.js";
-
-export {
-    HashbangRouter,
-    redirectHashbang,
-    Route,
-    Router,
-    Switch
-};
+export {default as Route} from "./route.js";
+export {default as Router} from "./router.js";
+export {default as Switch} from "./switch.js";
+export {HashbangRouter, redirectHashbang} from "./routing/hashbang.js";
+export {BrowserRouter, redirectBrowser} from "./routing/browser.js";
 
