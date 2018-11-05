d0ae7a5cd89ded073c4a106f8078f92d345317c5
jmjuanes <josemijuanes@gmail.com>
2018-11-05 16:43:35 +0100

    Minor changes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import React from "react";
+
+//Import rouct libs
 import {Context} from "./context.js";
-import unescape from "./commons/unescape.js";
+import unescape from "./utils/unescape.js";
+import * as paths from "./utils/paths.js";
 
 //Parse a query-string
 let parseQueryString = function (str) {
-    if (typeof str !== 'string'){ return {}; }
+    if (typeof str !== 'string') {
+        return {}; 
+    }
     //Initialize the output query object
     let query = {};
     str.trim().split("&").forEach(function (item) {
@@ -26,35 +31,26 @@ let parseQueryString = function (str) {
             query[itemKey] = itemValue;
         }
     });
+    //Return parsed query object
     return query;
 };
 
 //Export Router component 
 export default class Router extends React.Component {
     render() {
-        //Get the current location
-        let currentLocation = (typeof this.props.location === "string") ? this.props.location : "/";
+        //Get the current path
+        let currentPath = (typeof this.props.path === "string") ? paths.addLeadingSlash(this.props.path) : "/";
         //Initialize the request object
-        let request = {
-            "path": currentLocation,
-            "pathname": currentLocation,
-            "query": {}
-        };
-        //Check for query string values 
-        let queryIndex = request.path.indexOf("?");
-        if (queryIndex !== -1) {
-            //Parse the query string values
-            request.query = parseQueryString(request.path.substring(queryIndex + 1));
-            //Update the pathname 
-            request.pathname = request.path.slice(0, queryIndex);
-        }
+        let request = paths.parsePath(currenrPath);
+        //Add query string values
+        request.query = (request.search !== "") ? parseQueryString(request.search) : {};
         //Render the provider
-        return React.createElement(Context.Provider, {value: request}, this.props.children);
+        return React.createElement(Context.Provider, {"value": request}, this.props.children);
     }
 }
 
 //Router default props
 Router.defaultProps = {
-    "location": "/"
+    "path": "/"
 };
 
