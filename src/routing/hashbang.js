ac4e7b1bdfb24319ac615d96043b170f8fdd6cdb
jmjuanes <josemijuanes@gmail.com>
2018-11-05 16:21:49 +0100

    Add redirectHashbang redirect function

--- src/routing/hashbang.js
+++ src/routing/hashbang.js
@@ -19,7 +19,7 @@ let getCurrentHashbangPath = function () {
 };
 
 //Hashbang router component
-export default class HashbangRouter extends React.Component {
+export class HashbangRouter extends React.Component {
     constructor(props) {
         super(props);
         //Initialize the state with the current hashbang path
@@ -49,5 +49,23 @@ export default class HashbangRouter extends React.Component {
     render() {
         return React.createElement(Router, {"path": this.state.path}, this.props.children);
     }
+}
+
+//Hashbang redirect
+export function redirectHashbang (path) {
+    if (typeof path !== "string") {
+        //Invalid path
+        throw new Error("Expected string path in redirect method");
+    }
+    //Parse the path string --> trim the path 
+    let parsedPath = path.trim();
+    //while (parsedPath.charAt(0) === "#" || parsedPath.charAt(0) === "!") {
+    //    parsedPath = parsedPath.substring(1);
+    //}
+    //Check if the first character is not a slash mark
+    if (parsedPath.charAt(0) !== "/") {
+        parsedPath = "/" + parsedPath;
+    }
+    window.location.hash = "#!" + parsedPath;
 }
 
