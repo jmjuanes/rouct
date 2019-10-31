4aca0114d163d06afb45043c370531c03943f798
Josemi <josemijuanes@gmail.com>
2019-10-31 11:24:58 +0100

    Minor changes on route component

--- src/route.js
+++ src/route.js
@@ -4,30 +4,26 @@ import React from "react";
 import {Context} from "./context.js";
 import match from "./commons/match.js";
 
-//Route class
-export default class Route extends React.Component {
-    render() {
-        let self = this;
-        //Check for invalid component 
-        if (this.props.component === null) {
-            return null;
-        }
-        //Render the consumer component
-        return React.createElement(Context.Consumer, {}, function (value) {
-            //Check if the path of the route matches the current location
-            let result = match(value.pathname, self.props.path, self.props.exact);
-            if (result.matches === true) {
-                //Generate the request object
-                let request = Object.assign({"params": result.params}, value);
-                //Generate the new component props 
-                let props = Object.assign({"request": request}, self.props.props);
-                //Return the component
-                return React.createElement(self.props.component, props);
-            }
-            //If does not match, return null
-            return null;
-        });
+//Export route component
+export default function Route (props) {
+    if (props.component === null) {
+        return null;
     }
+    //Render the consumer component
+    return React.createElement(Context.Consumer, {}, function (value) {
+        //Check if the path of the route matches the current location
+        let result = match(value.pathname, props.path, props.exact);
+        if (result.matches === true) {
+            //Generate the request object
+            let request = Object.assign({"params": result.params}, value);
+            //Generate the new component props 
+            let props = Object.assign({"request": request}, props.props);
+            //Return the component
+            return React.createElement(props.component, props);
+        }
+        //If does not match, return null
+        return null;
+    });
 }
 
 //Route default props
