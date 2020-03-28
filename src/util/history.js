bb767c91059a9d6d68e8a69e1c0090b702cec083
Josemi <josemijuanes@gmail.com>
2020-03-28 20:55:17 +0100

    Moved history manager to util folder. Cahnged export type and made minor improvements

---
+++ src/util/history.js
@@ -0,0 +1,50 @@
+import * as paths from "./paths.js";
+
+//Get the current path
+let getCurrentPath = function () {
+    //Build the current path string
+    let currentPath = window.location.pathname + window.location.search + window.location.hash;
+    //Return the current path with a leading slash
+    return paths.addLeadingSlash(currentPath);
+};
+
+//History wrapper
+export function createHistory () {
+    let listener = null; //Initialize listener
+    //Trigger change listeners
+    let triggerChangeListener = function (newPath) {
+        if (state.listener !== null) {
+            return listener(newPath);
+        }
+    };
+    //Handle pop state listener
+    let handlePopState = function (event) {
+        return triggerChangeListener(getCurrentPath());
+    };
+    //Return managers
+    return {
+        //Register the popstate listener
+        "mount": function () {
+            return window.addEventListener("popstate", handlePopState, false);
+        },
+        //Remove the popstate listener
+        "unmount": function () {
+            return window.removeEventListener("popstate", handlePopState, false);
+        },
+        "getCurrentPath": getCurrentPath,
+        "push": function (newPath) {
+            newPath = paths.addLeadingSlash(newPath); //Add leading slash to the path
+            window.history.pushState({}, null, newPath); //Add to the history
+            return triggerChangeListener(newPath);
+        },
+        //Add path change listener
+        "addChangeListener": function (newListener) {
+            listener = newListener;
+        },
+        //Remove path change listener
+        "removeChangeListener": function () {
+            listener = null;
+        }
+    };
+}
+
