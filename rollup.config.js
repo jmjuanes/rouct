f47ff3dde67ae1a5d0b239c4cb5f5a7ca3e691f1
Josemi <josemijuanes@gmail.com>
2020-03-28 21:31:51 +0100

    Rewrite rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
-import commonjs from "rollup-plugin-commonjs";
-import replace from "@rollup/plugin-replace";
-import resolve from "rollup-plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+//import { terser } from "rollup-plugin-terser";
+//import replace from "@rollup/plugin-replace";
 
-//Initialize the configuration object
-let config = {
-    "input": "./src/index.js",
+//Export rollup config
+export default {
+    "input": "./index.js",
     "output": {
         "file": "./umd/rouct.js",
         "format": "umd",
@@ -17,19 +17,7 @@ let config = {
     "external": ["react"],
     "plugins": [
         resolve(),
-        commonjs(),
-        replace({
-            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-        })
+        commonjs()
     ]
 };
-
-//Check the node environment
-if (process.env.NODE_ENV === "production") {
-    config.output.file = "./umd/rouct.min.js";
-    config.plugins.push(terser());
-}
-
-//Export rollup configuration 
-export default config;
 
