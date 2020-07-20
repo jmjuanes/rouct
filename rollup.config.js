c1c149a0f11ba8a5e604d46dc28d63048174a0ac
Josemi <josemijuanes@gmail.com>
2020-07-20 20:07:27 +0200

    Added new exported formats (esm and cjs) and minified files

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,36 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-//import { terser } from "rollup-plugin-terser";
+import { terser } from "rollup-plugin-terser";
 //import replace from "@rollup/plugin-replace";
+
+//Generate output files
+let generateOutputFiles = function () {
+    return ["umd", "umd.min", "esm", "esm.min"].map(function (key) {
+        let output = {
+            "file": `./dist/rouct.${key}.js`,
+            "format": key.replace(".min", ""),
+            "globals": {
+                "react": "React"
+            }
+        };
+        //Check for umd export --> add name
+        if (key.startsWith("umd")) {
+            Object.assign(output, {"name": "Rouct"});
+        }
+        //Check for minimized file
+        if (key.endsWith(".min")) {
+            Object.assign(output, {"plugins": [terser()]});
+        }
+        //Return the output file config
+        return output;
+    });
+};
+
 
 //Export rollup config
 export default {
     "input": "./index.js",
-    "output": {
-        "file": "./umd/rouct.js",
-        "format": "umd",
-        "name": "Rouct",
-        "globals": {
-            "react": "React"
-        }
-    },
+    "output": generateOutputFiles(),
     "external": ["react"],
     "plugins": [
         resolve(),
